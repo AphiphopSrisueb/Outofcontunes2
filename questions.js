ef330463d71dcd16c038d71b80620519543d45d0
AphiphopSrisueb <88639628+AphiphopSrisueb@users.noreply.github.com>
2024-02-15 21:45:31 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "งานเราจัดที่โรงเรียนอะไร?",
+    answer: "โรงเรียนสาธิตแห่งมหาวิทยาลัยธรรมศาสตร์",
+    options: [
+      "โรงเรียนบ้านหนองกระโดน",
+      "ประถมธรรมศาสตร์",
+      "เตรียมอุดมศึกษา",
+      "โรงเรียนสาธิตแห่งมหาวิทยาลัยธรรมศาสตร์"
+    ]
+  },
+    {
+    numb: 2,
+    question: "งานเราจำลองสถานที่เป็นที่ไหน?",
+    answer: "บาร์",
+    options: [
+      "แถวบ้านแม่",
+      "บาร์",
+      "รัฐสภา",
+      "วัดสัปแมน"
+    ]
+  },
+  {
+    numb: 3,
+    question: "งานมีทั้งหมดกี่วง (รวมศิลปินนอก)",
+    answer: "10 วง",
+    options: [
+      "0.5 วง",
+      "9 วง",
+      "1ล้าน 5แสนวง",
+      "10 วง"
+    ]
+  },
+    {
+    numb: 4,
+    question: "งานมิวสิคเฟสปีนี้จัดเป็นปีที่เท่าไหร่?",
+    answer: "ปีที่ 4",
+    options: [
+      "ปีแรกงับ",
+      "ปีที่ 5",
+      "ปีที่แล้ว",
+      "ปีที่ 4"
+    ]
+  },
+  {      numb: 5,
+      question: "งานมิวสิคเฟสจัดเวลาใด?",
+      answer: "เวลาเที่ยงๆบ่ายๆ",
+      options: [
+        "เวลาดึกตอนเช้าตรู่",
+        "โพล้เพล้จนถึงเที่ยง",
+        "เวลาเที่ยงๆบ่ายๆ",
+        "เวลานั้นทีเราไม่ได้นัดกัน"
+      ]
+  },
+  {
+      numb: 6,
+      question: "วงไหนที่มีแต่ผู้หญิง?",
+      answer: "Girls on the beach",
+      options: [
+        "The Brothers",
+        "Girls do cry",
+        "Take Away",
+        "Girls on the beach"
+      ]
+  },
+  {
+      numb: 7,
+      question: "วงศิลปินนอกมีชื่ออะไรบ้าง?",
+      answer: "Loserpop กับ mute.",
+      options: [
+        "Loserpop กับ mute",
+        "Loserpunk กับ mu te",
+        "Loserpop กับ mute.",
+        "Lollipop กับ mute."
+      ]
+  },
+  {
+      numb: 8,
+      question: "คำโปรยคอนงานเราคืออะไร?",
+      answer: "We serve what you deserve",
+      options: [
+        "We don't serve what you like",
+        "I serve what I deserve ",
+        "We serve what you deserve",
+        "We serve for you na habibi "
+      ]
+  },
+  {
+      numb: 9,
+      question: "ทำไมต้องใช้ cocktail แทนวงดนตรี?",
+      answer: "เพราะเราอยากให้ทุกวงเป็นตัวเองได้เหมือนกับ cocktail ที่มีรสชาติ กลิ่น สี ที่ต่างกัน",
+      options: [
+        "เพราะนักดนตรีอยากกิน เวรี่หรอย อร่อยมากๆ หิวนำ้อร้ายสาสส สกิ้ปิ้ดีโด้ปๆเยสๆ",
+        "เพราะถ้าไม่ใช่ cocktail มันจะกลายเป็น crocodile ที่น่ากลัวสุดๆไปเลย เย่ๆๆ อร้ายๆๆๆๆ",
+        "เพราะเราอยากให้ทุกวงเป็นตัวเองได้เหมือนกับ cocktail ที่มีรสชาติ กลิ่น สี ที่ต่างกัน",
+        "เพราะเราอยากให้ทุกคนกินเครื่องดื่มและเมาไปกับ alcohol มาเป็น alcoholic ไปด้วยกัน"
+      ]
+  },
+  {
+      numb: 10,
+      question: "distillers ในงานมีหน้าที่ทำอะไร?",
+      answer: "เป็นทีมงาน",
+      options: [
+        "คนขัดห้องน้ำ",
+        "คอยชงเครื่องดื่ม",
+        "เป็นทีมงาน",
+        "เท่ๆ แอ็คๆตามงาน"
+      ]
+  }
+    
+];
