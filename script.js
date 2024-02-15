8e545e347fe8441636714aacde29e45ebc7ed2b2
AphiphopSrisueb <88639628+AphiphopSrisueb@users.noreply.github.com>
2024-02-15 22:38:34 +0700

    script.js

--- script.js
+++ script.js
@@ -166,34 +166,37 @@ function showResult() {
 
 
 
-function startTimer(time){
+function startTimer(time) {
     counter = setInterval(timer, 1000);
-    function timer(){
-        timeCount.textContent = time; //changing the value of timeCount with time value
-        time--; //decrement the time value
-        if(time < 9){ //if timer is less than 9
-            let addZero = timeCount.textContent; 
-            timeCount.textContent = "0" + addZero; //add a 0 before time value
+    function timer() {
+        if (time < 10) {
+            timeCount.textContent = "0" + time; // Add leading zero if time is less than 10
+        } else {
+            timeCount.textContent = time;
         }
-        if(time < 0){ //if timer is less than 0
-            clearInterval(counter); //clear counter
-            timeText.textContent = "Time End"; //change the time text to time off
-            const allOptions = option_list.children.length; //getting all option items
-            let correcAns = questions[que_count].answer; //getting correct answer from array
-            for(i=0; i < allOptions; i++){
-                if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer
-                    option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-                    option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
-                    console.log("Time Off: Auto selected correct answer.");
-                }
-            }
-            for(i=0; i < allOptions; i++){
-                option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
-            }
-            next_btn.classList.add("show"); //show the next button if user selected any option
+        time--; // decrement the time value
+
+        if (time < 0) { // if timer is less than 0
+            clearInterval(counter); // clear counter
+            timeText.textContent = "Time End"; // change the time text to Time End
+            // Disable all options
+            disableOptions();
         }
     }
 }
+
+
+function disableOptions() {
+    // Disable all option buttons
+    const optionButtons = document.querySelectorAll(".option");
+    optionButtons.forEach(button => {
+        button.classList.add("disabled");
+        button.removeAttribute("onclick");
+    });
+    // Show the next button to allow the player to proceed manually
+    next_btn.classList.add("show");
+}
+
 
 function startTimerLine(time){
     counterLine = setInterval(timer, 29);
